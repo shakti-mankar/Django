b5af715ff0af023db9e2a79737c96886c783d699
Shakti-mankar <mailshaktimankar@gmail.com>
2026-02-10 23:25:06 +0530

    new codes

--- model-relation/project/app/views.py
+++ model-relation/project/app/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
-
+from .models import *
 
 # Create your views here.
 
+def landing(req):
+    return render(req,'landing.html')
+
+
+def reverse_access(req):
+    data = Adhaar.objects.all()
+
+    for i in data:
+        print(
+            i.aadhar_no,
+            i.created_date,
+            i.created_by
+        )
+
+    return render(req, 'landing.html', {
+        "reverse_access": True,
+        "data": data
+    })
+
+def forward_access(req):
+    data = Student.objects.select_related('a_no').all()
+    print(data.query)
+
+    for i in data:
+        print(
+            i.name,
+            i.email,
+            i.contact,
+            i.city,
+            i.a_no.aadhar_no,
+            i.a_no.created_by
+        )
+
+    return render(req, 'landing.html', {
+        "forward_access": True,
+        "data": data
+    })
--- model-relation/project/project/urls.py
+++ model-relation/project/project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.landing,name='landing.html'),
+    path('reverse_access/',views.reverse_access,name='reverse_access'),
+    path('forward_access/',views.forward_access,name='forward_access'),
+
 ]
