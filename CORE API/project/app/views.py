468a521765f85704334ff39575bcfc7248f6dbdd
Shakti-mankar <mailshaktimankar@gmail.com>
2026-02-17 16:41:58 +0530

    emp_list in api

--- CORE API/project/app/views.py
+++ CORE API/project/app/views.py
@@ -24,3 +24,27 @@
 # p_data = json.loads(j_data)
 # print(p_data)
 # print(type(p_data))
+
+
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from .models import Employee
+import json
+
+
+def emp_list(req):
+    emp_data = Employee.objects.all()
+    print(emp_data)
+    p_data = list(emp_data.values())
+    print(p_data)
+    j_data = json.dumps(p_data)
+    print(j_data)
+    print(type(j_data))
+    return JsonResponse(j_data,safe=False)
+
+
+
+
+def detail(req,pk):
+    pass
