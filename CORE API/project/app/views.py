218226f8ba2933b1f82fcc4a4511ddeb929e0234
Shakti-mankar <mailshaktimankar@gmail.com>
2026-02-19 18:30:35 +0530

    core api complte

--- CORE API/project/app/views.py
+++ CORE API/project/app/views.py
@@ -26,6 +26,7 @@
 # print(type(p_data))
 
 
+
 from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 
@@ -55,7 +56,7 @@ def emp_list(req):
         # print(type(j_data))
         return HttpResponse(j_data,content_type='application/json')
 
-    emp_data = Employee.objects.all()
+    emp_data = Employee.objects.all()   
     print(emp_data)
     p_data = list(emp_data.values())
     print(p_data)
@@ -65,19 +66,66 @@ def emp_list(req):
     # return JsonResponse(j_data,safe=False) 
     return HttpResponse(j_data,content_type='application/json')
 
+@csrf_exempt
 def detail(req,pk):
-    if req.method=='PUT':
-        pass
-    elif req.method=='PATCH':
-        pass
-    elif req.method=='DELETE':
-        pass
-    emp_data = Employee.objects.get(id=pk)
-    print(emp_data)
-    p_data = model_to_dict(emp_data)
-    print(p_data)
-    j_data = json.dumps(p_data)
-    print(j_data)
-    # print(type(j_data))
-    # # return JsonResponse(j_data,safe=False) 
-    return HttpResponse(j_data,content_type='application/json')
+    user = Employee.objects.filter(id=pk)
+    if user:
+        if req.method=='PUT':
+            data = req.body
+            p_data = json.loads(data)
+            n,e,c,a = p_data.get('name'),p_data.get('email'),p_data.get('contact'),p_data.get('age')
+            # if n and e and c and a:
+            if 'name' in p_data and 'email' in p_data and 'contact' in p_data and 'age' in p_data:
+                old_obj = Employee.objects.get(id=pk)
+                old_obj.name = n
+                old_obj.email = e 
+                old_obj.contact = c
+                old_obj.age = a
+                old_obj.save()
+                d={'msg':'Object updated successfully.........'}
+                j_data = json.dumps(d)
+                return HttpResponse(j_data,content_type='application/json')
+            else:
+                d={'msg':'Some required filelds are missing'}
+                j_data = json.dumps(d)
+                return HttpResponse(j_data,content_type='application/json')
+
+        elif req.method=='PATCH':
+            data = req.body
+            p_data = json.loads(data)
+            if p_data:
+                # p_data = json.loads(data)
+                n,e,c,a = p_data.get('name'),p_data.get('email'),p_data.get('contact'),p_data.get('age')
+                old_obj = Employee.objects.get(id=pk)
+                if 'name' in p_data:
+                    old_obj.name = n
+                if 'email' in p_data:
+                    old_obj.email = e
+                if 'contact' in p_data:
+                    old_obj.contact = c
+                if 'age' in p_data:
+                    old_obj.age = a
+                old_obj.save()
+                d={'msg':'Object partially updated successfully.........'}
+                j_data = json.dumps(d)
+                return HttpResponse(j_data,content_type='application/json')
+            else:
+                d={'msg':'We need atleast one field to update but we don"t provide '}
+                j_data = json.dumps(d)
+                return HttpResponse(j_data,content_type='application/json')
+
+        elif req.method=='DELETE':
+            old_obj = Employee.objects.get(id=pk)
+            old_obj.delete()
+            d={'msg':'Object deleted successfully........!'}
+            j_data = json.dumps(d)
+            return HttpResponse(j_data,content_type='application/json')
+        emp_data = Employee.objects.get(id=pk)
+        p_data = model_to_dict(emp_data)
+        j_data = json.dumps(p_data)
+
+        return HttpResponse(j_data,content_type='application/json')
+    else:
+        d={'msg':'Given id is not present in our database'}
+        j_data = json.dumps(d)
+        return HttpResponse(j_data,content_type='application/json')
--- CORE API/project/project/urls.py
+++ CORE API/project/project/urls.py
@@ -20,8 +20,8 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('emp_list/',emp_list),
-    path('emp_detail/<int:pk>/',detail),
+    path('emp_list/',emp_list),                     # GET/POST
+    path('emp_detail/<int:pk>/',detail),            # GET/PUT/PATCH/DELETE
 
 
 
