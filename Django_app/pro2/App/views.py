fb0326c157953a2986b83d766007c49d06248b83
Shakti-mankar <mailshaktimankar@gmail.com>
2026-02-24 19:00:00 +0530

    Code update

--- Django_app/pro2/App/views.py
+++ Django_app/pro2/App/views.py
@@ -1,6 +1,102 @@
 from django.shortcuts import render , redirect
 
 # Create your views here.
+# from django.urls import reverse
+
+# from urllib.parse import urlencode
+
+
+
+# def my_redirect1(req):
+#     url = reverse('my_redirect2')
+#     data = urlencode({'name':'Neeraj' , 'age':37})
+
+#     return redirect (f'{url} ? {data}')
+
+
+
+# def my_redirect2(req):
+#     print("hello")
+#     print(req.get)
+
+
+
+
+from django.urls import reverse
+
+from urllib.parse import urlencode
+
+
+
+def my_redirect1(req):
+    url = reverse('my_redirect2')
+    data = urlencode({'name':'Neeraj' , 'age':37})
+
+    return redirect (f'{url} ? {data}')
+
+
+
+def my_redirect2(req):
+    print("hello")
+    print(req.get)
+
+
+
+
+
+
+
+from django.urls import reverse
+
+from urllib.parse import urlencode
+
+
+
+def my_redirect1(req):
+    url = reverse('my_redirect2')
+    data = urlencode({'name':'Neeraj' , 'age':37})
+
+    return redirect (f'{url} ? {data}')
+
+
+
+def my_redirect2(req):
+    print("hello")
+    print(req.get)
+
+
+
+
+
+
+
+
+
+
+
+from django.urls import reverse
+
+from urllib.parse import urlencode
+
+
+
+def my_redirect1(req):
+    url = reverse('my_redirect2')
+    data = urlencode({'name':'Neeraj' , 'age':37})
+
+    return redirect (f'{url} ? {data}')
+
+
+
+def my_redirect2(req):
+    print("hello")
+    print(req.get)
+
+
+
+
+
+
 from django.urls import reverse
 
 from urllib.parse import urlencode
