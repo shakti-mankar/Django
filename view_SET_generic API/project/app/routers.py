29a2bd24afd0b72cae8d3ab558cb45e875ff649c
Shakti-mankar <mailshaktimankar@gmail.com>
2026-03-08 10:00:00 +0530

    viewset api with generic

---
+++ view_SET_generic API/project/app/routers.py
@@ -0,0 +1,6 @@
+from .views import StudentViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'students', StudentViewSet, basename='student')
+urlpatterns = router.urls
