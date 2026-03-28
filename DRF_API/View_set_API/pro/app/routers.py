307c67644dfb451ba14448aa06c3a970a4b45741
Shakti-mankar <mailshaktimankar@gmail.com>
2026-03-28 10:00:00 +0530

    APi practice

--- DRF_API/View_set_API/pro/app/routers.py
+++
@@ -1,7 +0,0 @@
-from app.views import UserViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
-router.register(r'officers', UserViewSet, basename='officers')
-urlpatterns = router.urls
